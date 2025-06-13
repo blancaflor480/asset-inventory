974f837a067d31c6f00ccc1d727f30c66a6fa622
Jade Ryan Blancaflor <131427861+blancaflor480@users.noreply.github.com>
2025-06-13 21:30:40 +0800

    Update database.js

--- server/dist/config/database.js
+++ server/dist/config/database.js
@@ -8,10 +8,10 @@ const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'inventory_system',
+    host: process.env.DB_HOST || 'linen-snake-138010.hostingersite.com',
+    user: process.env.DB_USER || 'u408414471_admin',
+    password: process.env.DB_PASSWORD || 'Hakdog007@',
+    database: process.env.DB_NAME || 'u408414471_inventory',
 };
 exports.connection = promise_1.default.createConnection(dbConfig);
 //# sourceMappingURL=database.js.map
